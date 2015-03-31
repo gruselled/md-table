ce15d29a20e8573c7da06229bf46250d97c6a995
Dimitri <gruselled@gmail.com>
2015-04-01 00:05:05 +0200

    Mis e 14;59R jour du fichier js

--- dist/md-table.js
+++ dist/md-table.js
@@ -6,7 +6,8 @@
 angular.module('material.components.table', ['material.core'])
         .controller('mdTableController', ['$scope', '$filter', mdTableController])
         .directive('mdTable', mdTableDirective)
-        .filter('pageFilter', mdTablePageFilter);
+        .filter('pageFilter', mdTablePageFilter)
+        .filter('contentFilter', ['$filter', mdTableContentFilter]);
 
 /**
  * Create md-table Directive
@@ -30,101 +31,107 @@ function mdTableDirective() {
         link: mdTableLink,
         controller: mdTableController,
         template: '<div>\
-				<table class="md-table">\
-			<!-- Header -->\
-			<thead>\
-				<tr class="md-table-headers-row">\
-					<!-- Multiple selection -->\
-					<th class="md-table-header" ng-show="enableSelection">\
-						<!--\
-						<md-checkbox aria-label="Select all" on-change="selectAll(checked)"/>\
-						 -->\
-					</th>\
-					<!-- Content -->\
-					<th class="md-table-header" ng-repeat="header in headers" ng-class="headersClass[header.contentField]">\
-						<!-- Sortable Field -->\
-						<a href ng-if="header.sortableField" ng-click="sort(header.contentField, reverse);">\
-							{{header.label}}\
-							<span class="md-table-caret" ng-show="reverse && header.contentField == predicate">\
-								<img src="https://google.github.io/material-design-icons/navigation/svg/ic_arrow_drop_up_24px.svg">\
-							</span>\
-							<span class="md-table-caret" ng-show="!reverse && header.contentField == predicate">\
-								<img src="https://google.github.io/material-design-icons/navigation/svg/ic_arrow_drop_down_24px.svg">\
-							</span>\
-							<span class="unsorted" ng-show="!(header.contentField == predicate)">\
-							</span>\
-						</a>\
-						<!-- Unsortable Field -->\
-						<span ng-if="!header.sortableField">\
-							{{header.label}}\
-						</span>\
-					</th>\
-					<!-- Actions -->\
-					<th class="md-table-header" ng-show="enableAction" />\
-				</tr>\
-			</thead>\
-			<!-- Body -->\
-			<tbody>\
-				<tr class="md-table-content-row" ng-repeat="content in contents | filter: contentFilter | pageFilter: currentPage * pageCount | limitTo: pageCount">\
-					<!-- Selection -->\
-					<td class="md-table-td-check" ng-show="enableSelection">\
-						<md-checkbox aria-label="Select content" ng-model="checkedValue" ng-change="select({checked: checkedValue, selectedContent: content})"></md-checkbox>\
-					</td>\
-					<!-- Content -->\
-					<td ng-model="header" ng-repeat="header in headers">\
-						<div ng-switch="header.contentType">\
-							<!-- Thumb -->\
-							<div class="md-table-thumbs" ng-switch-when="image">\
-								<div style="background-image:url({{content[header.contentField]}})"></div>\
-							</div>\
-                            <!-- Input -->\
-                            <div class="md-table-content" ng-switch-when="input">\
-                                <input ng-model="content[header.contentField]" />\
-                            </div>\
-                            <!-- Text -->\
-                            <div class="md-table-content" ng-switch-when="text" ng-class="contentsClass[header.contentField]">\
-                            {{content[header.contentField] | contentFilter : header.contentFilter}}\
-                            </div>\
-						</div>\
-					</td>\
-					<!-- Actions -->\
-					<td class="md-table-td-more" ng-show="enableAction">\
-						<md-button class="md-action" ng-click="action({selectedContent: content})" aria-label="Action">\
-							<img src="http://google.github.io/material-design-icons/navigation/svg/ic_more_vert_24px.svg" />\
-						</md-button>\
-					</td>\
-				</tr>\
-			</tbody>\
-		</table>\
-		<!-- Footer / Pagination -->\
-		<div class="md-table-footer" layout="row">\
-			<span flex></span>\
-			<span ng-show="enablePagination">\
-				<!-- Previous page -->\
-				<md-button aria-label="Previous" class="md-table-footer-item" ng-click="previous()" ng-disabled="previousDisabled();">\
-					<img src="http://google.github.io/material-design-icons/hardware/svg/ic_keyboard_arrow_left_24px.svg" />\
-				</md-button>\
-				<!-- Current page selection -->\
-				<a class="md-table-page-link" href ng-repeat="page in pages">\
-					<md-button aria-label="Index" class="md-primary md-table-footer-item" ng-click="selectPage(page.index)">\
-						<span ng-class="{\'md-table-active-page\': currentPage == page.index}">{{page.index}}</span>\
-					</md-button>\
-				</a>\
-				<!-- Next page -->\
-				<md-button aria-label="Next" class="md-table-footer-item" ng-click="next()" ng-disabled="nextDisabled();">\
-					<img src="http://google.github.io/material-design-icons/hardware/svg/ic_keyboard_arrow_right_24px.svg" />\
-				</md-button>\
-			</span>\
-		</div>\
-	</div>'
+<table class="md-table">\
+        <!-- Header -->\
+    <thead>\
+    <tr class="md-table-headers-row">\
+        <!-- Multiple selection -->\
+    <th class="md-table-header" ng-show="enableSelection">\
+        <!-- \
+        <md-checkbox aria-label="Select all" on-change="selectAll(checked)"/>\
+         -->\
+    </th>\
+        <!-- Content -->\
+    <th class="md-table-header" ng-repeat="header in headers" ng-class="headersClass[header.contentField]">\
+        <!-- Sortable Field -->\
+    <a href ng-if="header.sortableField" ng-click="sort(header.contentField, reverse);">\
+    {{header.label}}\
+<span class="md-table-caret" ng-show="reverse && header.contentField == predicate">\
+<img src="https://google.github.io/material-design-icons/navigation/svg/ic_arrow_drop_up_24px.svg">\
+</span>\
+<span class="md-table-caret" ng-show="!reverse && header.contentField == predicate">\
+<img src="https://google.github.io/material-design-icons/navigation/svg/ic_arrow_drop_down_24px.svg">\
+</span>\
+<span class="unsorted" ng-show="!(header.contentField == predicate)">\
+</span>\
+</a>\
+    <!-- Unsortable Field -->\
+<span ng-if="!header.sortableField">\
+{{header.label}}\
+</span>\
+</th>\
+    <!-- Actions -->\
+<th class="md-table-header" ng-show="enableAction" />\
+</tr>\
+</thead>\
+    <!-- Body -->\
+<tbody>\
+<tr class="md-table-content-row" ng-repeat="content in contents | filter: contentFilter | pageFilter: currentPage * pageCount | limitTo: pageCount">\
+    <!-- Selection -->\
+<td class="md-table-td-check" ng-show="enableSelection">\
+<md-checkbox aria-label="Select content" ng-model="checkedValue" ng-change="select({checked: checkedValue, selectedContent: content})"></md-checkbox>\
+</td>\
+    <!-- Content -->\
+<td ng-model="header" ng-repeat="header in headers">\
+<div ng-switch="header.contentType">\
+    <!-- Thumb -->\
+<div class="md-table-thumbs" ng-switch-when="image">\
+<div style="background-image:url({{content[header.contentField]}})"></div>\
+</div>\
+    <!-- Input -->\
+<div class="md-table-content" ng-switch-when="input">\
+<input ng-model="content[header.contentField]" />\
+</div>\
+    <!-- Text -->\
+<div class="md-table-content" ng-switch-when="text" ng-class="contentsClass[header.contentField]">\
+{{content[header.contentField] | contentFilter : header.contentFilter}}\
+</div>\
+\
+    </div>\
+    </td>\
+        <!-- Actions -->\
+    <td class="md-table-td-more" ng-show="enableAction">\
+    <md-button class="md-action" ng-click="action({selectedContent: content})" aria-label="Action">\
+    <img src="http://google.github.io/material-design-icons/navigation/svg/ic_more_vert_24px.svg" />\
+    </md-button>\
+    </td>\
+    </tr>\
+    </tbody>\
+    </table>\
+\
+        <!-- Footer / Pagination -->\
+    <div class="md-table-footer" layout="row">\
+    <span flex></span>\
+    <span ng-show="enablePagination">\
+        <!-- Previous page -->\
+    <md-button aria-label="Previous" class="md-table-footer-item" ng-click="previous()" ng-disabled="previousDisabled();">\
+    <img src="http://google.github.io/material-design-icons/hardware/svg/ic_keyboard_arrow_left_24px.svg" />\
+    </md-button>\
+        <!-- Current page selection -->\
+    <a class="md-table-page-link" href ng-repeat="page in pages">\
+    <md-button aria-label="Index" class="md-primary md-table-footer-item" ng-click="selectPage(page.index)">\
+    <span ng-class="{\'md-table-active-page\': currentPage == page.index}">{{page.index}}</span>\
+</md-button>\
+</a>\
+    <!-- Next page -->\
+<md-button aria-label="Next" class="md-table-footer-item" ng-click="next()" ng-disabled="nextDisabled();">\
+<img src="http://google.github.io/material-design-icons/hardware/svg/ic_keyboard_arrow_right_24px.svg" />\
+</md-button>\
+</span>\
+</div>\
+</div>'
     };
 }
 
 /**
  * Create mdTableController
+ * 
+ * @param {type} $scope
+ * @param {type} $filter
+ * @returns {undefined}
  */
 function mdTableController($scope, $filter) {
-    
+
     initializeControllerDatas($scope);
     initializePagination($scope);
 
@@ -211,6 +218,27 @@ function mdTablePageFilter() {
 }
 
 /**
+ * @return The filter use to paginate
+ */
+function mdTableContentFilter($filter) {
+    return function (value, contentFilter) {
+        if(contentFilter) {
+            if(contentFilter.pattern) {
+                var result = $filter(contentFilter.filter)(value, contentFilter.pattern);
+                console.log(result);
+                return result; 
+            } else {
+                var result = $filter(contentFilter.filter)(value);
+                console.log(result);
+                return result;
+            }
+        } else {
+            return value;
+        }
+    };
+}
+
+/**
  * Initialization of controller's data
  */
 function initializeControllerDatas($scope) {
@@ -219,6 +247,7 @@ function initializeControllerDatas($scope) {
     $scope.predicate = '';
     // Pagination
     $scope.currentPage = 0;
+    initializePagination($scope);
 }
 
 /**
@@ -251,8 +280,8 @@ function initializePagination($scope) {
  * @param contentType 
  *            Nature of content's field (Default => 'text' :
  *            - 'input'
- *			  - 'text' 
- *			  - 'image'            
+ *            - 'text' 
+ *            - 'image'            
  * @param sortableField 
  *            Flag to indicate if the field is sortable
  */
